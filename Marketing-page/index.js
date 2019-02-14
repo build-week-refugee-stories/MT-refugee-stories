a5b3597705700c3d6c011f2e4568d810a8320a2b
Marc Thiongo <marcthuo@gmail.com>
2019-02-14 08:46:32 -0500

    js additions

--- Marketing-page/index.js
+++ Marketing-page/index.js
@@ -1,10 +1,24 @@
 
-const p = document.querySelector(".p");
-p.addEventListener("mouseover", e => {   
+const pel = document.querySelector(".pel");
+pel.addEventListener("mouseover", e => {   
     // highlight the mouseover target
     e.target.style.color = "orange";
 
+
   setTimeout(function() {
-      event.target.style.color = "";
+      
     }, 100);
   }, false);
+
+const war = document.querySelector(".war");
+war.addEventListener("mouseover", e => {   
+    // highlight the mouseover target
+    e.target.style.opacity = "0.6";
+});
+
+
+const nav = document.querySelector("nav");
+nav.addEventListener("mouseover", e => {   
+    // highlight the mouseover target
+    e.target.style.background = "white";
+});
--- Menu/menu.js
+++ Menu/menu.js
@@ -1,13 +1,16 @@
 
 const toggleMenu = () => {
-    
     menu.classList.toggle('menu--over');
   }
-  
   const menu = document.querySelector('.menu');
- 
   const menuButton = document.querySelector('.menu-button');
- 
+
   menuButton.addEventListener('click', () => {
     toggleMenu();
-  })
+  });
+
+  const menudrop = document.querySelector(".menu");
+  menudrop.addEventListener("mouseover", e => {   
+      // highlight the mouseover target
+      e.target.style.opacity = "0.8";
+  });
